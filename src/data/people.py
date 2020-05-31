d2e176de70b4745bff47307383b86095c2064d99
Miriam <miriam9495@gmail.com>
2020-05-31 16:21:34 +0200

    multiprocessing working version 1

--- src/data/people.py
+++ src/data/people.py
@@ -1,129 +1,87 @@
+##################################################### S E L E N I U M
+
 from selenium import webdriver
+from multiprocessing import Pool, cpu_count
 import time
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dbModel import Base, Article
 import os
 
-from multiprocessing.pool import ThreadPool
-import threading
-from multiprocessing import Pool, cpu_count
+####################################################### A L C H E M Y
 
-"""
-def people():
+from dbModel import Base, Article
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('sqlite:///' + '../../data/raw/prueba.db')
+Session = sessionmaker(bind=engine)
 
-    print('Connecting to db...')
-    engine = create_engine('sqlite:///' + '../../data/raw/prueba.db')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    articles = session.query(Article).all()
+######################################################################
 
-    for i,article in enumerate(articles):
+def getPeople(link):
 
-        if not article.People:
+    try:
 
-           try:
-                #driver = webdriver.Chrome('./chromedriver')
-                link = article.Link
-                driver.get(link)
-                time.sleep(10)
+        driver = webdriver.Chrome('./chromedriver')
+        driver.get(link)
+        time.sleep(3)
 
-                element = driver.find_elements_by_tag_name('h4.aq.cl.as.ds')
-                element[0].click()
-                time.sleep(3)
-                people = driver.find_elements_by_tag_name('h2.aq.ds')
+        element = driver.find_elements_by_tag_name('button')
+        element[2].click()
+        time.sleep(2)
+        people = driver.find_elements_by_tag_name('h2')
 
-                for e in people:
-                    if 'people' in e.text:
-                        article.People=int(e.text.split(' ')[3])
-                        session.commit()
-                        print(f"Added People in article...{i} | Result: {e.text.split(' ')[3]}")
+        for e in people:
 
-                #driver.quit()
+            if 'people' in e.text:
 
-           except KeyboardInterrupt:
-                print('Exiting')
-                os._exit(status=0)
+                #Open session
+                session = Session()
 
-           except:
-               pass
+                #Find article
+                article = session.query(Article).filter_by(Link=link).scalar()
 
-    session.close()
+                article.People = int(e.text.split(' ')[3])
+                print(f"Added in article...{article.id} | People: {article.People}")
 
-if __name__ == '__main__':
-    people()
-    
-"""
+                session.commit()
+                session.close()
 
-def getPeople(datalist):
+                """ Doesn't work - why?
+                with engine.connect() as connection:
+                    article = connection.execute(f" SELECT * FROM articles WHERE Link = '{link}' ")
+                    #article.People = int(e.text.split(' ')[3])
+                    #print(f"Added in article...{article.id} | People: {article.People}")
+                """
 
-    for article in datalist:
+        driver.quit()
 
-        try:
-            driver = webdriver.Chrome('./chromedriver')
-            driver.get(article.Link)
-            time.sleep(10)
-
-            element = driver.find_elements_by_tag_name('h4.aq.cl.as.ds')
-            element[0].click()
-            time.sleep(3)
-            people = driver.find_elements_by_tag_name('h2.aq.ds')
-
-            for e in people:
-                if 'people' in e.text:
-                    session.begin_nested()
-                    article.People = int(e.text.split(' ')[3])
-                    print(f"Added in article...{article.id} | People: {article.People}")
-
-            driver.quit()
-
-        except Exception as e:
-            print(e)
-            pass
-
+    except Exception as e:
+        print(e)
+        pass
 
 def run_parallel_selenium_processes(datalist, selenium_func):
 
     pool = Pool()
 
-    ITERATION_COUNT = cpu_count()-1
+    ITERATION_COUNT = cpu_count()-1 # 4 - 1
 
     count_per_iteration = len(datalist) / float(ITERATION_COUNT)
 
-    for i in range(0, ITERATION_COUNT):
-        list_start = int(count_per_iteration * i)
-        list_end = int(count_per_iteration * (i+1))
+    for i in range(0, ITERATION_COUNT):                   #     0  to 12405 -> CPU 1
+        list_start = int(count_per_iteration * i)         #  12405 to 24811 -> CPU 2
+        list_end = int(count_per_iteration * (i+1))       #  12405 to 24811 -> CPU 2
 
-        #     0  to 12405 -> CPU 1
-        #  12405 to 24811 -> CPU 2
-        #  24811 to 37217 -> CPU 3
-
-        # pool.apply_async(f, args) --> Multi-args YES / Concurrence  YES / Blocking NO / Ordered-results NO
-        pool.apply_async(selenium_func, datalist[list_start:list_end])
-
+        # pool.map(f, args) --> Multi-args NO / Concurrence  YES / Blocking YES / Ordered-results YES
+        pool.map(selenium_func, datalist[list_start:list_end])
 
 def people():
 
     print('Connecting to db...')
 
-    #Create engine
-    engine = create_engine('sqlite:///' + '../../data/raw/prueba.db')
+    with engine.connect() as connection:
+        articles = connection.execute('SELECT * FROM articles')
+        links = [article.Link for article in articles if not article.People]
 
-    #Create a configured session class
-    Session = sessionmaker(bind=engine)
-
-    #Create an instance
-    session = Session()
-
-    #Query to obtain articles
-    articles = session.query(Article).all()
-
-    #Multiprocessing
     try:
-        run_parallel_selenium_processes(datalist=articles, selenium_func=getPeople)
-        session.commit()
-        session.close()
+        run_parallel_selenium_processes(datalist=links, selenium_func=getPeople)
 
     except KeyboardInterrupt:
         print('Exiting')
