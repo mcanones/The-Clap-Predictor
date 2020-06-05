e5c9637bd87bc83d2ba65eea3e3caf5e70fe317d
Miriam <miriam9495@gmail.com>
2020-06-05 11:43:31 +0200

    definitive script for scraping people data

--- src/data/people.py
+++ src/data/people.py
@@ -3,14 +3,19 @@
 from selenium import webdriver
 from multiprocessing import Pool, cpu_count
 import time
+from dbModel import Article
+
 import os
+os.environ['PATH'] = f'{os.environ["PATH"]}:{os.getcwd()}/drivers'
+from selenium.webdriver.firefox.options import Options
+options = Options()
+options.headless = True
 
-####################################################### A L C H E M Y
+####################################################### A L C H E M Y 
 
-from dbModel import Base, Article
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///' + '../../data/raw/prueba.db')
+engine = create_engine('sqlite:///' + '../../data/raw/people.db')
 Session = sessionmaker(bind=engine)
 
 ######################################################################
@@ -19,12 +24,12 @@ def getPeople(link):
 
     try:
 
-        driver = webdriver.Chrome('./chromedriver')
+        driver = webdriver.Firefox(options=options)
         driver.get(link)
-        time.sleep(3)
+        time.sleep(5)
 
-        element = driver.find_elements_by_tag_name('button')
-        element[2].click()
+        element = driver.find_elements_by_tag_name('h4 button')
+        element[1].click()
         time.sleep(2)
         people = driver.find_elements_by_tag_name('h2')
 
@@ -37,37 +42,34 @@ def getPeople(link):
 
                 #Find article
                 article = session.query(Article).filter_by(Link=link).scalar()
-
-                article.People = int(e.text.split(' ')[3])
-                print(f"Added in article...{article.id} | People: {article.People}")
+                
+                article.People = int(e.text.split('people')[0].split(' ')[-2])
+                #article.People = int(e.text.split(' ')[3])
+                print(f"Added in article...{article.id} | People: {article.People} | Link: {article.Link}")
 
                 session.commit()
                 session.close()
-
-                """ Doesn't work - why?
-                with engine.connect() as connection:
-                    article = connection.execute(f" SELECT * FROM articles WHERE Link = '{link}' ")
-                    #article.People = int(e.text.split(' ')[3])
-                    #print(f"Added in article...{article.id} | People: {article.People}")
-                """
-
+            
         driver.quit()
 
     except Exception as e:
         print(e)
+        driver.quit()
         pass
 
 def run_parallel_selenium_processes(datalist, selenium_func):
+    
+    #datalist = datalist[6000:]
 
     pool = Pool()
 
-    ITERATION_COUNT = cpu_count()-1 # 4 - 1
+    ITERATION_COUNT = cpu_count()-1 
 
     count_per_iteration = len(datalist) / float(ITERATION_COUNT)
 
-    for i in range(0, ITERATION_COUNT):                   #     0  to 12405 -> CPU 1
-        list_start = int(count_per_iteration * i)         #  12405 to 24811 -> CPU 2
-        list_end = int(count_per_iteration * (i+1))       #  12405 to 24811 -> CPU 2
+    for i in range(0, ITERATION_COUNT):                   
+        list_start = int(count_per_iteration * i)         
+        list_end = int(count_per_iteration * (i+1))      
 
         # pool.map(f, args) --> Multi-args NO / Concurrence  YES / Blocking YES / Ordered-results YES
         pool.map(selenium_func, datalist[list_start:list_end])
@@ -89,6 +91,5 @@ def people():
 
 if __name__ == '__main__':
     people()
-
 
 
